d29860b9a08598c99877026b400ea71f37d7636f
pedrohqds <souzapedrohde@outlook.com>
2024-04-16 07:54:33 -0300

    Testing and starting Pyspark

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import pandas as pd
+from pyspark.sql import SparkSession
 
-print(f"Teste Python: {pd.__version__}")
+spark = SparkSession.builder.appName("Teste").getOrCreate()
+
+_options = {
+    "header": "true",
+    "delimiter": ";"
+}
+df = spark.read.format("csv").options(**_options).load("./csv_test.csv")
+
+df.show()
+
+spark.stop()
